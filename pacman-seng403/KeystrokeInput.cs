ab8524cdd90b5728cff89315f0fa514e8da474cc
nshaken <nshaken@71960494-1b4a-11df-8fc8-8521ee8a6493>
2010-03-13 03:16:34 +0000

    Everything except MyPacman is satisfactory for Iteration 1 deliverables.  Need to fix MyPacman if possible.

---
+++ pacman-seng403/KeystrokeInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pacman
+{
+    class KeystrokeInput
+    {
+
+        /*
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            
+        }
+        */
+    }
+}
---
+++ pacman-seng403/StupidGhost.cs
@@ -0,0 +1,115 @@
+
+using System;
+
+namespace pacman
+{
+	
+	
+	public class StupidGhost: Ghost
+	{
+        public StupidGhost(int start_x, int start_y, char dir) {
+            x = start_x*10 + 5;
+            y = start_y*10 + 5;
+            direction = dir; //starting direction is up
+		}
+		
+		public void screenUpdate() {
+            int mapCurrentPosition = (int)Map.getMapEntry((int)x/10, (int)y/10);
+            if (direction == 'u') {
+                if ((mapCurrentPosition & (int)Directions.UP) == (int)Directions.UP) {
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.RIGHT) == (int)Directions.RIGHT) {
+                    direction = 'r';
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.LEFT) == (int)Directions.LEFT) {
+                    direction = 'l';
+                    move(direction);
+                }
+	    	    
+                else if ((mapCurrentPosition & (int)Directions.DOWN) == (int)Directions.DOWN) {
+                    direction = 'd';
+                    move(direction);
+                }
+            }
+            else if (direction == 'd') {
+                if ((mapCurrentPosition & (int)Directions.DOWN) == (int)Directions.DOWN) {
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.RIGHT) == (int)Directions.RIGHT) {
+                    direction = 'r';
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.LEFT) == (int)Directions.LEFT) {
+                    direction = 'l';
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.UP) == (int)Directions.UP) {
+                    direction = 'u';
+                    move(direction);
+                }
+            }
+            else if (direction == 'l') {
+                if ((mapCurrentPosition & (int)Directions.LEFT) == (int)Directions.LEFT) {
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.UP) == (int)Directions.UP) {
+                    direction = 'u';
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.DOWN) == (int)Directions.DOWN) {
+                    direction = 'd';
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.RIGHT) == (int)Directions.RIGHT) {
+                    direction = 'r';
+                    move(direction);
+                }
+            }
+            else if (direction == 'r') {
+                if ((mapCurrentPosition & (int)Directions.RIGHT) == (int)Directions.RIGHT) {
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.UP) == (int)Directions.UP) {
+                    direction = 'u';
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.DOWN) == (int)Directions.DOWN) {
+                    direction = 'd';
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.LEFT) == (int)Directions.LEFT) {
+                    direction = 'l';
+                    move(direction);
+                }
+            }
+            else {
+                //ERROR - ghost is not moving
+                if ((mapCurrentPosition & (int)Directions.RIGHT) == (int)Directions.RIGHT) {
+                    direction = 'r';
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.UP) == (int)Directions.UP) {
+                    direction = 'u';
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.DOWN) == (int)Directions.DOWN) {
+                    direction = 'd';
+                    move(direction);
+                }
+                else if ((mapCurrentPosition & (int)Directions.LEFT) == (int)Directions.LEFT) {
+                    direction = 'l';
+                    move(direction);
+                }
+                else {
+                    //ERROR - ghost is in a wall
+		            //reset position to center
+		            x = 250;
+                    y = 250;
+                }
+            }
+            return;
+		}
+	}
+}
