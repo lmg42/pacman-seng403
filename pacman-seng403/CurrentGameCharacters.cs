ea2f46b397b00ffe4162f8a5bd2903e5a745e97a
nasrullahs <nasrullahs@71960494-1b4a-11df-8fc8-8521ee8a6493>
2010-03-25 15:58:27 +0000

    Added CurrentGameCharacters class that has references to pacman and ghosts and dots and fruits

---
+++ pacman-seng403/CurrentGameCharacters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pacmanSENG403
+{
+    class CurrentGameCharacters
+    {
+        public static MyPacman pacman;
+        public static Ghost inky;
+        public static Ghost blinky;
+        public static Ghost pinky;
+        public static Ghost clyde;
+    }
+}
