728b79e6b9c4783c32b1405acbc5e18aee3875cd
nasrullahs <nasrullahs@71960494-1b4a-11df-8fc8-8521ee8a6493>
2010-03-25 16:07:48 +0000

    updated CurrentGameCharacters

--- trunk/pacman-seng403/CurrentGameCharacters.cs
+++ trunk/pacman-seng403/CurrentGameCharacters.cs
@@ -8,9 +8,16 @@ namespace pacmanSENG403
     class CurrentGameCharacters
     {
         public static MyPacman pacman;
+        
         public static Ghost inky;
         public static Ghost blinky;
         public static Ghost pinky;
         public static Ghost clyde;
+
+        public static List<Edibles> dots_topright = new List<Edibles>();
+        public static List<Edibles> dots_topleft = new List<Edibles>();
+        public static List<Edibles> dots_bottomright = new List<Edibles>();
+        public static List<Edibles> dots_bottomleft = new List<Edibles>();
+        public static List<Edibles> bigdots = new List<Edibles>();
     }
 }
