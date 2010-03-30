9484e2eb5ca3946a8a08c8685d5a0c3d2cac2651
nasrullahs <nasrullahs@71960494-1b4a-11df-8fc8-8521ee8a6493>
2010-03-30 18:09:41 +0000

    Updated Ghost to handle variable block size instead of hard-coded value.

--- trunk/pacman-seng403/Ghost.cs
+++ trunk/pacman-seng403/Ghost.cs
@@ -9,6 +9,7 @@ namespace pacman
 	{
 		public static readonly double SPEED = 1.0;
         protected char direction;
+        protected bool weak;
 
 		public Ghost() {
 			radius = 4;
@@ -18,22 +19,22 @@ namespace pacman
 
         protected void move(char dir) {
             if (dir == 'u') {
-                x = (double)((((int)x) / 10)*10 + 5);
+                x = (double)((((int)x) / Map.BLOCKSIZE) * Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
                 y -= SPEED;
                 return;
             }
             else if (dir == 'd') {
-                x = (double)((((int)x) / 10)*10 + 5);
+                x = (double)((((int)x) / Map.BLOCKSIZE)*Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
                 y += SPEED;
                 return;
             }
             else if (dir == 'l') {
-                y = (double)((((int)y) / 10)*10 + 5);
+                y = (double)((((int)y) / Map.BLOCKSIZE)*Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
                 x -= SPEED;
                 return;
             }
             else if (dir == 'r') {
-                y = (double)((((int)y) / 10)*10 + 5);
+                y = (double)((((int)y) / Map.BLOCKSIZE)*Map.BLOCKSIZE + (Map.BLOCKSIZE));
                 x += SPEED;
                 return;
             }
