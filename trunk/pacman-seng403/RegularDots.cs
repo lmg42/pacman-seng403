eded37e82af3b8c06907c0fa6d2231039d4267e5
aeshaa412 <aeshaa412@71960494-1b4a-11df-8fc8-8521ee8a6493>
2010-04-11 22:55:02 +0000

    Small Dots

---
+++ trunk/pacman-seng403/RegularDots.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pacman
+{
+    class RegularDots
+    { 
+        public RegularDots()
+        {
+            for(int i= 0 ; i < 13 ; i++ ) {
+                for (int j = 0; j < 13; j++)
+                {
+                    if (Map.getMapEntry(i, j) != Directions.NONE)
+                    {
+                        Edibles e = new Edibles(((i * 20) + 30), ((j * 20) + 30), "smallDot");
+                        CurrentGameCharacters.dots_topleft.Add(e);
+                    }
+                }
+            }
+            for (int i = 0; i < 13; i++)
+            {
+                for (int j = 13; j < 25; j++)
+                {
+                    if(Map.getMapEntry(i,j) != Directions.NONE) {
+                        Edibles e = new Edibles(((i * 20) + 30), ((j * 20) + 30), "smallDot");
+                        CurrentGameCharacters.dots_topright.Add(e);
+                    }
+                }
+            }
+            for (int i = 13; i < 25; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    if (Map.getMapEntry(i, j) != Directions.NONE)
+                    {
+                        Edibles e = new Edibles(((i * 20) + 30), ((j * 20) + 30), "smallDot");
+                        CurrentGameCharacters.dots_bottomleft.Add(e);
+                    }
+                }
+            }
+            for (int i = 13; i < 25; i++)
+            {
+                for (int j = 13; j < 25; j++)
+                {
+                    if (Map.getMapEntry(i, j) != Directions.NONE)
+                    {
+                        Edibles e = new Edibles(((i * 20) + 30), ((j * 20) + 30), "smallDot");
+                        CurrentGameCharacters.dots_bottomright.Add(e);
+                    }
+                }
+            }     
+        }
+    }
+}
