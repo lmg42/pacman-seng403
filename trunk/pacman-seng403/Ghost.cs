0aeeca94d669b5ae446aa8f125135b041b286482
nasrullahs <nasrullahs@71960494-1b4a-11df-8fc8-8521ee8a6493>
2010-04-11 22:28:47 +0000

    Integrated SmartGhost and StupidGhost into one Ghost class.

--- trunk/pacman-seng403/Ghost.cs
+++ trunk/pacman-seng403/Ghost.cs
@@ -5,21 +5,34 @@ namespace pacman
 {
 	
 	
-	public abstract class Ghost: GameCharacter
+	public class Ghost: GameCharacter
 	{
 		public static readonly double SPEED = 1.0;
         protected Directions direction;
         protected bool weak=false;
+        protected bool smart;
+        protected int numScreenUpdates;
 
-		public Ghost() {
+        public Ghost(int start_x, int start_y, Directions dir, bool isSmart)
+        {
+            smart = isSmart;
 			radius = 4;
+            x = start_x*Map.BLOCKSIZE + (Map.BLOCKSIZE/2);
+            y = start_y*Map.BLOCKSIZE + (Map.BLOCKSIZE/2);
+            direction = dir;
+            numScreenUpdates = 20;
 		}
 
         public void makeWeak() {
             weak = true;
         }
 
-        public Boolean isWeak() {
+        public void makeStrong()
+        {
+            weak = false;
+        }
+
+        public bool isWeak() {
             return weak;
         }
 
@@ -33,55 +46,208 @@ namespace pacman
             }
         }
 
-        public abstract void screenUpdate();
+        public void screenUpdate()
+        {
+            if (smart && !weak)
+            {
+                if (numScreenUpdates < 20)
+                {
+                    move(direction, false);
+                    numScreenUpdates++;
+                }
+                else if (numScreenUpdates == 20)
+                {
+                    //find direction to pacman
+                    Directions mapCurrentPosition = Map.getMapEntry((int)x / Map.BLOCKSIZE, (int)y / Map.BLOCKSIZE);
+                    Directions dirtopacman = Directions.NONE;
+                    if (CurrentGameCharacters.pacman.getX() < this.x)
+                    {
+                        dirtopacman = Directions.LEFT;
+                    }
+                    else if (CurrentGameCharacters.pacman.getX() > this.x)
+                    {
+                        dirtopacman = Directions.RIGHT;
+                    }
+                    if (CurrentGameCharacters.pacman.getY() < this.y)
+                    {
+                        dirtopacman = (dirtopacman | Directions.UP);
+                    }
+                    else if (CurrentGameCharacters.pacman.getY() > this.y)
+                    {
+                        dirtopacman = (dirtopacman | Directions.DOWN);
+                    }
 
-        /*
-        //trying to make ghost turning smoother
-        protected void move(Directions dir, Directions previous) {
-            if (dir == Directions.UP) {
-                if((x%10) < 3) {
-                    
-                }
-                else if ((x%10) > 7) {
+                    //move ghost in direction closest to pacman
+                    if (((dirtopacman & Directions.RIGHT) == Directions.RIGHT) && ((mapCurrentPosition & Directions.RIGHT) == Directions.RIGHT))
+                    {
+                        direction = Directions.RIGHT;
+                        move(direction, true);
+                    }
+                    else if (((dirtopacman & Directions.LEFT) == Directions.LEFT) && ((mapCurrentPosition & Directions.LEFT) == Directions.LEFT))
+                    {
+                        direction = Directions.LEFT;
+                        move(direction, true);
+                    }
+                    else if (((dirtopacman & Directions.UP) == Directions.UP) && ((mapCurrentPosition & Directions.UP) == Directions.UP))
+                    {
+                        direction = Directions.UP;
+                        move(direction, true);
+                    }
+                    else if (((dirtopacman & Directions.DOWN) == Directions.DOWN) && ((mapCurrentPosition & Directions.DOWN) == Directions.DOWN))
+                    {
+                        direction = Directions.DOWN;
+                        move(direction, true);
+                    }
+                    else
+                    { //cant move toward pacman
+                        if ((mapCurrentPosition & Directions.UP) == Directions.UP)
+                        {
+                            direction = Directions.UP;
+                            move(direction, true);
+                        }
+                        else if ((mapCurrentPosition & Directions.RIGHT) == Directions.RIGHT)
+                        {
+                            direction = Directions.RIGHT;
+                            move(direction, true);
+                        }
+                        else if ((mapCurrentPosition & Directions.LEFT) == Directions.LEFT)
+                        {
+                            direction = Directions.LEFT;
+                            move(direction, true);
+                        }
 
+                        else if ((mapCurrentPosition & Directions.DOWN) == Directions.DOWN)
+                        {
+                            direction = Directions.DOWN;
+                            move(direction, true);
+                        }
+                    }
+                    numScreenUpdates = 0;
                 }
-                else {
-                    x = (double)((((int)x) / Map.BLOCKSIZE) * Map.BLOCKSIZE + (Map.BLOCKSIZE / 2));
-                    y -= SPEED;
+            }
+            else //not smart
+            {
+                if (numScreenUpdates < 20)
+                {
+                    move(direction, false);
+                    numScreenUpdates++;
                 }
-            }
-            else if (dir == Directions.DOWN) {
-                x = (double)((((int)x) / Map.BLOCKSIZE) * Map.BLOCKSIZE + (Map.BLOCKSIZE / 2));
-                y += SPEED;
-            }
-            else if (dir == Directions.LEFT) {
-                y = (double)((((int)y) / Map.BLOCKSIZE) * Map.BLOCKSIZE + (Map.BLOCKSIZE / 2));
-                x -= SPEED;
-            }
-            else if (dir == Directions.RIGHT) {
-                y = (double)((((int)y) / Map.BLOCKSIZE) * Map.BLOCKSIZE + (Map.BLOCKSIZE / 2));
-                x += SPEED;
+                else if (numScreenUpdates == 20)
+                {
+                    Directions mapCurrentPosition = Map.getMapEntry((int)x / Map.BLOCKSIZE, (int)y / Map.BLOCKSIZE);
+                    Directions dir = Directions.NONE; //all possible directions ghost can move in
+                    if (direction == Directions.UP)
+                    {
+                        if ((mapCurrentPosition & Directions.RIGHT) == Directions.RIGHT)
+                        {
+                            dir = (dir | Directions.RIGHT);
+                        }
+                        if ((mapCurrentPosition & Directions.LEFT) == Directions.LEFT)
+                        {
+                            dir = (dir | Directions.LEFT);
+                        }
+                        if ((mapCurrentPosition & Directions.UP) == Directions.UP)
+                        {
+                            dir = (dir | Directions.UP);
+                        }
+                    }
+                    else if (direction == Directions.DOWN)
+                    {
+                        if ((mapCurrentPosition & Directions.RIGHT) == Directions.RIGHT)
+                        {
+                            dir = (dir | Directions.RIGHT);
+                        }
+                        if ((mapCurrentPosition & Directions.LEFT) == Directions.LEFT)
+                        {
+                            dir = (dir | Directions.LEFT);
+                        }
+                        if ((mapCurrentPosition & Directions.DOWN) == Directions.DOWN)
+                        {
+                            dir = (dir | Directions.DOWN);
+                        }
+                    }
+                    else if (direction == Directions.LEFT)
+                    {
+                        if ((mapCurrentPosition & Directions.DOWN) == Directions.DOWN)
+                        {
+                            dir = (dir | Directions.DOWN);
+                        }
+                        if ((mapCurrentPosition & Directions.UP) == Directions.UP)
+                        {
+                            dir = (dir | Directions.UP);
+                        }
+                        if ((mapCurrentPosition & Directions.LEFT) == Directions.LEFT)
+                        {
+                            dir = (dir | Directions.LEFT);
+                        }
+                    }
+                    else if (direction == Directions.RIGHT)
+                    {
+                        if ((mapCurrentPosition & Directions.DOWN) == Directions.DOWN)
+                        {
+                            dir = (dir | Directions.DOWN);
+                        }
+                        if ((mapCurrentPosition & Directions.UP) == Directions.UP)
+                        {
+                            dir = (dir | Directions.UP);
+                        }
+                        if ((mapCurrentPosition & Directions.RIGHT) == Directions.RIGHT)
+                        {
+                            dir = (dir | Directions.RIGHT);
+                        }
+                    }
+                    //randomly choose one of the possible directions
+                    //if chosen direction is impossible, choose different direction
+                    Random random = new Random();
+                    int shiftBy = random.Next(0, 4);
+
+                    for (int i = 0; i <= 4; i++)
+                    {
+                        if ((((int)dir >> shiftBy) & 0x1) == 0x1)
+                        {
+                            move((Directions)(0x1 << shiftBy), true);
+                            break;
+                        }
+                        else
+                        {
+                            shiftBy = ((shiftBy + 1) % 5);
+                        }
+                    }
+
+                    numScreenUpdates = 0;
+                }
+                else
+                {
+                    //this should never happen
+                    //put error handling code here
+                    Console.WriteLine("StupidGhost.numScreenUpdates is over 20");
+                }
             }
         }
-        */
 
-        protected void move(Directions dir) {
+        protected void move(Directions dir, bool center) {
+            direction = dir;
             if (dir == Directions.UP) {
-                x = (double)((((int)x) / Map.BLOCKSIZE) * Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
+                if(center)
+                    x = (double)((((int)x) / Map.BLOCKSIZE) * Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
                 y -= SPEED;
             }
             else if (dir == Directions.DOWN) {
-                x = (double)((((int)x) / Map.BLOCKSIZE)*Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
+                if(center)
+                    x = (double)((((int)x) / Map.BLOCKSIZE)*Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
                 y += SPEED;
             }
             else if (dir == Directions.LEFT) {
-                y = (double)((((int)y) / Map.BLOCKSIZE)*Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
+                if(center)
+                    y = (double)((((int)y) / Map.BLOCKSIZE)*Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
                 x -= SPEED;
             }
             else if (dir == Directions.RIGHT) {
-                y = (double)((((int)y) / Map.BLOCKSIZE)*Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
+                if(center)
+                    y = (double)((((int)y) / Map.BLOCKSIZE)*Map.BLOCKSIZE + (Map.BLOCKSIZE/2));
                 x += SPEED;
             }
         }
+
 	}
 }
