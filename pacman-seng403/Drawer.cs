8ceb222e57f13a377720063958846ae5c803d81a
lisa.graham42 <lisa.graham42@71960494-1b4a-11df-8fc8-8521ee8a6493>
2010-04-12 22:08:24 +0000

    sleep reverted; rectangle moved down (score, etc)

--- pacman-seng403/Drawer.cs
+++ pacman-seng403/Drawer.cs
@@ -327,8 +327,8 @@ namespace pacman
                         }
 
                         //create rectangle at the bottom so we can write score, lives, and level over top
-                        e.Graphics.DrawRectangle(new Pen(Color.Chocolate), 0, 480, 500, 20);
-                        e.Graphics.FillRectangle(Brushes.Chocolate, 0, 480, 500, 20);
+                        e.Graphics.DrawRectangle(new Pen(Color.Chocolate), 0, 480, 505, 20);
+                        e.Graphics.FillRectangle(Brushes.Chocolate, 0, 480, 505, 20);
 
                         //score
                         e.Graphics.DrawString("Score: " + GameData.score, new Font("Ouhod", 12, FontStyle.Bold), Brushes.Yellow, 2, 480);
@@ -367,7 +367,7 @@ namespace pacman
                     {
 
                     }
-                    Thread.Sleep(5);
+                    Thread.Sleep(10);
                 }
             }
 
