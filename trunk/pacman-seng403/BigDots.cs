23128cbfda03beff6663ad5136886d3c501bbc1d
aeshaa412 <aeshaa412@71960494-1b4a-11df-8fc8-8521ee8a6493>
2010-04-12 19:17:59 +0000

    BigDots Class defining the location for bigdots.  I have four big dots for now, one in each quadrant.

---
+++ trunk/pacman-seng403/BigDots.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pacman
+{
+    class BigDots
+    {
+        public BigDots()
+        {
+            Edibles a = new Edibles(130,170,"bigDot");
+            Edibles b = new Edibles(370,170,"bigDot");
+            Edibles c = new Edibles(190,350 ,"bigDot");
+            Edibles d = new Edibles(470,470,"bigDot");
+            CurrentGameCharacters.bigdots.Add(a);
+            CurrentGameCharacters.bigdots.Add(b);
+            CurrentGameCharacters.bigdots.Add(c);
+            CurrentGameCharacters.bigdots.Add(d);
+        }
+    }
+}
